706ebb62abedb6a7604ccf79db772c0dc371f9ff
alemas <mateusreckziegel@gmail.com>
2021-10-21 21:21:57 -0300

    Added ingredients and instructions to Drink Model

--- CocktailMakr/Models/Drink.swift
+++ CocktailMakr/Models/Drink.swift
@@ -9,25 +9,22 @@ import Foundation
 
 struct Drink {
 
+    struct RecipeIngredient {
+        let name: String
+        let measure: String?
+    }
+    
     // MARK: Properties
     
     let id: String
     let name: String
     let isAlcoholic: Bool
+    let instructions: String
+    let ingredients: [RecipeIngredient]
     let category: String?
     let glassType: String?
     let imageURL: URL?
     
-    enum CodingKeys: String, CodingKey {
-        
-        case id = "idDrink"
-        case name = "strDrink"
-        case isAlcoholic = "strAlcoholic"
-        case category = "strCategory"
-        case glassType = "strGlass"
-        case imageURL = "strDrinkThumb"
-        
-    }
 }
 
 // MARK: Decodable
@@ -39,8 +36,25 @@ extension Drink: Decodable {
         id = try values.decode(String.self, forKey: CodingKeys.id)
         name = try values.decode(String.self, forKey: CodingKeys.name)
         isAlcoholic = try values.decode(String.self, forKey: CodingKeys.isAlcoholic) == "Alcoholic"
-        category = try values.decode(String.self, forKey: CodingKeys.category)
-        glassType = try values.decode(String.self, forKey: CodingKeys.glassType)
-        imageURL = try values.decode(URL.self, forKey: CodingKeys.imageURL)
+        instructions = try values.decode(String.self, forKey: CodingKeys.instructions)
+        category = try? values.decode(String.self, forKey: CodingKeys.category)
+        glassType = try? values.decode(String.self, forKey: CodingKeys.glassType)
+        imageURL = try? values.decode(URL.self, forKey: CodingKeys.imageURL)
+        
+        var tempIngredients = [RecipeIngredient]()
+        
+        for i in (1...15) {
+            let ingredientKey = CodingKeys.init(rawValue: "strIngredient\(i)")!
+            guard let ingredientName = try? values.decode(String.self, forKey: ingredientKey) else { break }
+            
+            let measureKey = CodingKeys.init(rawValue: "strMeasure\(i)")!
+            let measure = try? values.decode(String.self, forKey: measureKey)
+            
+            tempIngredients.append(RecipeIngredient(name: ingredientName, measure: measure))
+        }
+        
+        ingredients = tempIngredients
+        
     }
 }
+
---
+++ CocktailMakr/Models/Extensions/Drink+CodingKeys.swift
@@ -0,0 +1,55 @@
+//
+//  DrinkCodingKeys+Ingredients.swift
+//  CocktailMakr
+//
+//  Created by Mateus Reckziegel on 21/10/21.
+//
+
+import Foundation
+
+extension Drink {
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id = "idDrink"
+        case name = "strDrink"
+        case isAlcoholic = "strAlcoholic"
+        case instructions = "strInstructions"
+        case category = "strCategory"
+        case glassType = "strGlass"
+        case imageURL = "strDrinkThumb"
+        
+        case ingredient1 = "strIngredient1"
+        case ingredient2 = "strIngredient2"
+        case ingredient3 = "strIngredient3"
+        case ingredient4 = "strIngredient4"
+        case ingredient5 = "strIngredient5"
+        case ingredient6 = "strIngredient6"
+        case ingredient7 = "strIngredient7"
+        case ingredient8 = "strIngredient8"
+        case ingredient9 = "strIngredient9"
+        case ingredient10 = "strIngredient10"
+        case ingredient11 = "strIngredient11"
+        case ingredient12 = "strIngredient12"
+        case ingredient13 = "strIngredient13"
+        case ingredient14 = "strIngredient14"
+        case ingredient15 = "strIngredient15"
+        
+        case measure1 = "strMeasure1"
+        case measure2 = "strMeasure2"
+        case measure3 = "strMeasure3"
+        case measure4 = "strMeasure4"
+        case measure5 = "strMeasure5"
+        case measure6 = "strMeasure6"
+        case measure7 = "strMeasure7"
+        case measure8 = "strMeasure8"
+        case measure9 = "strMeasure9"
+        case measure10 = "strMeasure10"
+        case measure11 = "strMeasure11"
+        case measure12 = "strMeasure12"
+        case measure13 = "strMeasure13"
+        case measure14 = "strMeasure14"
+        case measure15 = "strMeasure15"
+    }
+    
+}
